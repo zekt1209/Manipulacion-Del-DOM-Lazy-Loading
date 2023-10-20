4d7834b9aaa578e03fdc7700d451961e3a00d4c9
Victor Guzman <victor_27edge@hotmail.com>
2023-10-20 00:49:49 -0600

    Agregamos los eventos de los botones agregarImagen y limpiarImagenes

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import * as nodes from './nodes'
 /**
  * This file is just a silly example to show everything working in the browser.
  * When you're ready to start on your site, clear the file. Happy hacking!
@@ -5,8 +6,9 @@
 
 console.log('Happy hacking :)');
 
-
+// Utils
 const API = 'https://randomfox.ca/floof';
+
 
 const fetchData = async (url) => {
     
@@ -24,18 +26,24 @@ const fetchData = async (url) => {
     } catch (error) {
         console.error('Error en peticion de fetchData, error: ' + error);
     }
-
+    
 }
 
-{/* <div class="app_imageContainer"> 
-<img class="app_image" src="https://randomfox.ca/images/84.jpg" alt="fox image">
-</div> */}
 
 const getRandomFoxes = async (url) => {
     const data = await fetchData(url);
     
-    const imageUrl = data.image;
+    /*
+    {"image": "https://randomfox.ca/images/44.jpg",
+    "link": "https://randomfox.ca/?i=44" }
+    */
+   
+   const imageUrl = data.image;
 
+   {/* <div class="app_imageContainer"> 
+   <img class="app_image" src="https://randomfox.ca/images/84.jpg" alt="fox image">
+   </div> */}
+   
     // Maquetamos el HTML desde dentro hacia afuera para ir insertando los child
     const img = document.createElement('img');
     img.setAttribute('src', imageUrl);
@@ -51,7 +59,19 @@ const getRandomFoxes = async (url) => {
 
 }
 
+// EventListeners
+nodes.agregarBtn.addEventListener('click', () => {
+    getRandomFoxes(API);
+});
+
+
+nodes.limpiarBtn.addEventListener('click', () => {
+    nodes.app.innerHTML = "";
+})
+
+
+
 // fetchData(API);
 getRandomFoxes(API);
-getRandomFoxes(API);
-getRandomFoxes(API);
+// getRandomFoxes(API);
+// getRandomFoxes(API);
---
+++ src/nodes.js
@@ -0,0 +1,6 @@
+// Containers
+export const app = document.querySelector('#app');
+
+// Elements
+export const agregarBtn = document.querySelector('#controls_agregarBtn');
+export const limpiarBtn = document.querySelector('#controls_limpiarBtn');
